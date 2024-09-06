2ab8cd4ccca848d755209d8c665112962e9aab61
Rodrigo <rodrigo@ite.edu.br>
2024-09-06 19:09:11 -0300

    Aula04

--- aula_template/routes/web.php
+++ aula_template/routes/web.php
@@ -1,15 +1,34 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/' , function() {
-   return view("principal.index");
+/*
+php artisan migrate:fresh
+
+*/
+
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('produtos/', function() {
-    return view('produtos.lista');
+Route::get('/produtos', [ProdutosController::class, 'index']);
+Route::get('/produtos/criar', [ProdutosController::class, 'novo']);
+
+
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/gerencia', function() {
-    return view('admin.principal.index');
-});
+require __DIR__.'/auth.php';
