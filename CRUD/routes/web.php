cbaaf7f5720accd24a0a2cccecde7f69ebea9ee5
Rodrigo <rodrigo@ite.edu.br>
2024-10-18 19:28:55 -0300

    login.html

--- CRUD/routes/web.php
+++ CRUD/routes/web.php
@@ -4,27 +4,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProdutoController;
 
-Route::get("/", function() {
-    return view("admin_template.index");
+Route::middleware('auth')->group(function () {
+    Route::get("/categoria", [ CategoriaController::class, "index" ] );
+    Route::post("/categoria", [ CategoriaController::class, "SalvarNovaCategoria" ]);
+
+    Route::get("/categoria/upd/{id}", 
+        [CategoriaController::class, 'BuscaAlterar']
+    )->name('cat_alterar');
+    Route::post("categoria/udp",
+        [CategoriaController::class, 'SalvarAlteracao']
+    )->name('cat_alt_salva');
+    Route::get("/categoria/exc/{id}", 
+        [ CategoriaController::class, 'ExcluirCategoria']
+    )->name('cat_excluir');
+
+
+    Route::get("/produto", [ProdutoController::class, 'index'] )->name('produto_idex');
+    Route::post("/produto", 
+        [ProdutoController::class, 'SalvarNovoProduto'] )->name('produto_novo');
 });
-
-Route::get("/categoria", [ CategoriaController::class, "index" ] );
-Route::post("/categoria", [ CategoriaController::class, "SalvarNovaCategoria" ]);
-
-Route::get("/categoria/upd/{id}", 
-    [CategoriaController::class, 'BuscaAlterar']
-)->name('cat_alterar');
-Route::post("categoria/udp",
-    [CategoriaController::class, 'SalvarAlteracao']
-)->name('cat_alt_salva');
-
-
-Route::get("/categoria/exc/{id}", 
-    [ CategoriaController::class, 'ExcluirCategoria']
-)->name('cat_excluir');
-
-
-Route::get("/produto", [ProdutoController::class, 'index'] )->name('produto_idex');
-
-Route::post("/produto", 
-    [ProdutoController::class, 'SalvarNovoProduto'] )->name('produto_novo');
